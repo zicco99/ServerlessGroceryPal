9e3157768c295b588e51c2952f87f7176f817af7
Zicco99 <f.ziccolella@protonmail.com>
2024-04-27 18:37:20 +0200

    npm install and module init

--- backend/lambdas/nest_js_serverless/index.ts
+++ backend/lambdas/nest_js_serverless/index.ts
@@ -17,6 +17,11 @@ const initializeNestApp = async () => {
         const zip = new AdmZip(Body);
         zip.extractAllTo('/tmp/nestjs');
 
+        //Move the package.json to the dist folder and install the dependencies
+        const { execSync } = require('child_process');
+        execSync("mv /tmp/nestjs/backend/api/package.json /tmp/nestjs/backend/api/dist", { stdio: 'inherit' });
+        execSync('npm install --prefix /tmp/nestjs/backend/api/dist', { stdio: 'inherit' });
+
         const { AppModule } = require('/tmp/nestjs/backend/api/dist/app.module');
         const nestApp = await NestFactory.create(AppModule, new ExpressAdapter(express()));
 

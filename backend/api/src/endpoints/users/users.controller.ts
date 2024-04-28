1ae565038904c891a4b6d1496c80a3118237d426
Zicco99 <f.ziccolella@protonmail.com>
2024-04-28 11:00:36 +0200

    removing authmodule

--- backend/api/src/endpoints/users/users.controller.ts
+++ backend/api/src/endpoints/users/users.controller.ts
@@ -10,10 +10,7 @@ import {
   Request,
   UseFilters,
 } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { LoginUserDto } from './dto/login-user.dto';
-import { AuthService } from 'src/auth/auth.service';
 import { AuthGuard } from '@nestjs/passport';
 import { UsersService } from './users.service';
 import { Sequelize } from 'sequelize-typescript';
@@ -23,54 +20,9 @@ import { DBExceptionFilter } from 'src/error_handling/db.exception.filter';
 @Controller('users')
 export class UsersController {
   constructor(
-    private authService: AuthService,
     private userService: UsersService,
     private sequelize: Sequelize,
   ) {}
-
-  @Post('/login')
-  async login(@Body() credentials: LoginUserDto) {
-    // checks if the email already exists
-    const user = await this.authService.validateUserOnLogin(credentials);
-
-    // if it does exist it uses passport to log in
-    const token = await this.authService.login(user, user.id);
-    // then returns the token
-    return token;
-  }
-
-  @Post('/create')
-  async create(@Body() createUserDto: CreateUserDto) {
-    // checks if the email already exists, if exists calls login, if not it goes on
-    await this.authService.validateUserOnCreate(createUserDto);
-
-    // converts the dto in the db user object
-    const convertedUser = this.userService.convertNewUser(createUserDto);
-
-    const transaction = await this.sequelize.transaction();
-
-    // creates the record on db
-    const user = await this.userService.createUserOnDb(
-      convertedUser,
-      transaction,
-    );
-
-    // converts the dto in the db user config object
-    const convertedUserConfig = this.userService.convertNewUserConfig(
-      createUserDto,
-      user.id,
-    );
-    // creates the record on db
-    await this.userService.createUserConfigOnDb(
-      convertedUserConfig,
-      transaction,
-    );
-
-    await transaction.commit();
-
-    // calls the login function and returns the token
-    return HttpStatus.CREATED;
-  }
 
   @UseGuards(AuthGuard('jwt'))
   @Put('/config')
--- backend/api/src/endpoints/users/users.module.ts
+++ backend/api/src/endpoints/users/users.module.ts
@@ -2,13 +2,12 @@ import { Module, Logger } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { UserTable } from 'src/db/models/user.model';
-import { AuthService } from 'src/auth/auth.service';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { UserConfigTable } from 'src/db/models/user-config.model';
 
 @Module({
   imports: [SequelizeModule.forFeature([UserTable, UserConfigTable])],
   controllers: [UsersController],
-  providers: [UsersService, AuthService, Logger],
+  providers: [UsersService, Logger],
 })
 export class UsersModule {}

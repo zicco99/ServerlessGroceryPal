66f63c101365d11b760ef2feffec3dbe17fcabcf
Zicco99 <f.ziccolella@protonmail.com>
2024-05-03 21:36:26 +0200

    compile db client within lambda compilation

--- backend/api/src/recipes/recipes.service.ts
+++ backend/api/src/recipes/recipes.service.ts
@@ -1,7 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma.service';
 
 @Injectable()
 export class RecipesService {
+  constructor(private readonly prismaService: PrismaService) {}
 
-    
+  async getAllRecipes() {
+    return await this.prismaService.recipe.findMany();
+  }
+
+  async getRecipeById(id: number) {
+    return await this.prismaService.recipe.findUnique({
+      where: { id },
+    });
+  }
+
+  async deleteRecipe(id: number) {
+    return await this.prismaService.recipe.delete({
+      where: { id },
+    });
+  }
 }

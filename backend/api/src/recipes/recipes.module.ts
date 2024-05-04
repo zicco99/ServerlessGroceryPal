265eb91cb50b7189785d378b6edc1256842da7e0
Zicco99 <f.ziccolella@protonmail.com>
2024-05-04 12:19:16 +0200

    following nest js concepts to give project a structure

--- backend/api/src/recipes/recipes.module.ts
+++ backend/api/src/recipes/recipes.module.ts
@@ -3,9 +3,10 @@ import { RecipesController } from './recipes.controller';
 import { RecipesService } from './recipes.service';
 
 @Module({
-  imports: [], 
-  controllers: [RecipesController], // declare the controllers that this module uses ()
-  providers: [RecipesService], // declare the components (services, providers, etc.) that are managed by the Nest.js dependency injection container within the current module or the module that imports this module
-  exports: [RecipesService, RecipesController], // exports the component (service, provider, controller, etc.) from this module so that other modules can use it.
+  controllers: [RecipesController],
+  providers: [RecipesService],
+  exports: [RecipesController, RecipesService], // Export controllers and services
 })
 export class RecipesModule {}
+
+

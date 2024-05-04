8fd37fa4a0b26b86c7d2eb9f2e731920e25b7fa7
Zicco99 <f.ziccolella@protonmail.com>
2024-05-04 15:38:26 +0200

    avoid controller export

--- backend/api/src/prisma/prisma.module.ts
+++ backend/api/src/prisma/prisma.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 
 @Module({
-  providers: [PrismaService],
-  exports: [PrismaService],
+  providers: [PrismaService], // Defines PrismaService as a provider within this module
+  exports: [PrismaService], // Exports PrismaService to make it available for dependency injection in other modules
 })
 export class PrismaModule {}
--- backend/api/src/recipes/recipes.module.ts
+++ backend/api/src/recipes/recipes.module.ts
@@ -1,14 +1,12 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { RecipesController } from './recipes.controller';
 import { RecipesService } from './recipes.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
-    imports: [],
-    controllers: [RecipesController],
-    providers: [RecipesService],
-    exports: [RecipesService], // Export controllers and services
+  imports: [forwardRef(() => PrismaModule)],
+  controllers: [RecipesController],
+  providers: [RecipesService],
+  exports: [RecipesService]
 })
 export class RecipesModule {}
-
-
-

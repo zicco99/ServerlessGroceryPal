2487cb197ba3e785fafd967b8c83c7d262a4c1d0
Zicco99 <f.ziccolella@protonmail.com>
2024-05-04 16:02:03 +0200

    connection override using lambda env

--- backend/api/src/prisma/prisma.service.ts
+++ backend/api/src/prisma/prisma.service.ts
@@ -3,6 +3,16 @@ import { PrismaClient } from '../../prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  constructor() {
+    super({
+      datasources: {
+        db: {
+          url: `postgresql://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+        },
+      },
+    });
+  }
+
   async onModuleInit() {
     await this.$connect();
   }

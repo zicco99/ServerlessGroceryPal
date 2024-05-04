9f17921d1cbedfdff3e0bb7a4b14e3f7bfcc8979
Zicco99 <f.ziccolella@protonmail.com>
2024-05-04 16:17:57 +0200

    connection override using lambda env

--- backend/api/src/prisma/prisma.service.ts
+++ backend/api/src/prisma/prisma.service.ts
@@ -1,16 +1,8 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '../../prisma/client';
-import { Prisma } from '../../prisma/client/default';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
-  constructor(optionsArg?: Prisma.Subset<Prisma.PrismaClientOptions, Prisma.PrismaClientOptions>) {
-    if(process.env.ENV === 'cloud') {
-      optionsArg.datasourceUrl = `postgresql://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
-    }
-    super(optionsArg);
-  }
-
   async onModuleInit() {
     await this.$connect();
   }

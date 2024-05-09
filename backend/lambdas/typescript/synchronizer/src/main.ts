70fe2d69ec558bc03a92766085bcd912bc4a13d8
Zicco99 <f.ziccolella@protonmail.com>
2024-05-09 08:44:36 +0200

    adding a http response

--- backend/lambdas/typescript/synchronizer/src/main.ts
+++ backend/lambdas/typescript/synchronizer/src/main.ts
@@ -5,5 +5,12 @@ export const handler: Handler = async (
   context: Context,
   callback: Callback,
 ) => {
-  return 'Hello from Lambda!';
+  const response = {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ message: 'Hello from Lambda!' }),
+  };
+  return response;
 };

5a185abf52b5bc9a7e499b9c60c7c0a4a0f0dc7a
Zicco99 <f.ziccolella@protonmail.com>
2024-05-11 21:50:15 +0200

    fix

--- backend/lambdas/typescript/synchronizer/webpack.config.ts
+++ backend/lambdas/typescript/synchronizer/webpack.config.ts
@@ -1,64 +1,40 @@
-import * as webpack from 'webpack';
+import { Configuration } from 'webpack';
 import CopyPlugin from 'copy-webpack-plugin';
 import path from 'path';
 
-module.exports = function (options: webpack.Configuration): webpack.Configuration {
-    const lazyImports = [
-        'aws-lambda',
-        'aws-sdk',
-        'axios',
-        'cheerio',
-        'prisma',
-    ];
-
-    return {
-        ...options,
-        entry: ['./src/lambda.ts'],
-        externals: [],
-        output: {
-            ...options.output,
-            libraryTarget: 'commonjs2',
-        },
-        plugins: [
-            ...(options.plugins || []),
-            new webpack.IgnorePlugin({
-                checkResource(resource: string) {
-                    return lazyImports.includes(resource);
+const webpackConfig: Configuration = {
+    entry: './src/lambda.ts',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js',
+        library: '[name]',
+        libraryTarget: 'commonjs2',
+    },
+    target: 'node',
+    devtool: 'source-map',
+    externals: [],
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: 'src/prisma/client',
+                    to: 'prisma/client',
                 },
-            }),
-            new CopyPlugin({
-                patterns: [
-                    {
-                        from: 'src/prisma/client',
-                        to: 'prisma/client',
-                    },
-                ],
-            })
+            ],
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
         ],
-        optimization: {
-            runtimeChunk: true,
-            splitChunks: {
-                chunks: 'all'
-            }
-        },
-        resolve: {
-            // Add '.ts' as a resolvable extension.
-            extensions: ['.ts', '.js']
-        },
-        module: {
-            rules: [
-                // TypeScript files
-                {
-                    test: /\.ts$/,
-                    use: 'ts-loader',
-                    exclude: /node_modules/
-                },
-                // CSS files
-                {
-                    test: /\.css$/,
-                    use: ['style-loader', 'css-loader']
-                }
-            ]
-        }
-    };
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
 };
+
+export default webpackConfig;

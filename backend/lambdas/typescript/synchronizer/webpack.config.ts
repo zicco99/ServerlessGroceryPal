5a2f1e8dcf4f98edf5508e433d01f0783bafa344
Zicco99 <f.ziccolella@protonmail.com>
2024-05-11 21:03:32 +0200

    bundling app + lazy import

--- backend/lambdas/typescript/synchronizer/webpack.config.ts
+++ backend/lambdas/typescript/synchronizer/webpack.config.ts
@@ -1,30 +1,50 @@
-const path = require('path');
-const webpack = require('webpack');
+import * as webpack from 'webpack';
+import * as CopyPlugin from 'copy-webpack-plugin';
 
-module.exports = {
-    entry: './src/lambda.ts', // Update entry point to lambda.ts
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].js',
-        library: '[name]',
-        libraryTarget: 'commonjs2',
-    },
-    target: 'node',
-    devtool: 'source-map',
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
+module.exports = function (options: webpack.Configuration): webpack.Configuration {
+    const lazyImports = [
+        'aws-lambda',
+        'aws-sdk',
+        'axios',
+        'cheerio',
+        'prisma',
+    ];
+
+    return {
+        ...options,
+        entry: ['./src/lambda.ts'],
+        externals: [],
+        output: {
+            ...options.output,
+            libraryTarget: 'commonjs2',
+        },
+        plugins: [
+            ...(options.plugins || []),
+            new webpack.IgnorePlugin({
+                checkResource(resource: string) {
+                    return lazyImports.includes(resource);
+                },
+            }),
+            new CopyPlugin({
+                patterns: [
+                    {
+                        from: 'src/prisma/client',
+                        to: 'prisma/client',
+                    },
+                ],
+            })
         ],
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-    },
-    // Work around "Error: Can't resolve 'pg-native'"
-    plugins: [
-        new webpack.IgnorePlugin(/^pg-native$/)
-    ],
+        optimization: {
+            runtimeChunk: true,
+            splitChunks: {
+                chunks: 'all'
+            }
+        },
+        module: {
+            rules: [
+                { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+                { test: /\.ts$/, use: 'ts-loader', exclude: /node_modules/ }
+            ]
+        }
+    };
 };

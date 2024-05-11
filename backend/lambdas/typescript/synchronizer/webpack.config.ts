ba52eb6c4c543a810f3e4f92fac61c12003038cf
Zicco99 <f.ziccolella@protonmail.com>
2024-05-12 00:03:03 +0200

    bundling app + lazy import

--- backend/lambdas/typescript/synchronizer/webpack.config.ts
+++ backend/lambdas/typescript/synchronizer/webpack.config.ts
@@ -3,7 +3,7 @@ import nodeExternals from 'webpack-node-externals';
 
 module.exports = {
     mode: 'production',
-    entry: './src/main.ts',
+    entry: 'src/main.ts',
     target: 'node',
     externals: [nodeExternals()],
     module: {

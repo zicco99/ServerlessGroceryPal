e19559b713732e918964b7a480ea4938795ff1e1
Zicco99 <f.ziccolella@protonmail.com>
2024-05-12 19:32:54 +0200

    removing dist injection

--- backend/lambdas/typescript/synchronizer/webpack.config.ts
+++ backend/lambdas/typescript/synchronizer/webpack.config.ts
@@ -1,5 +1,6 @@
 import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
 import { CleanWebpackPlugin } from 'clean-webpack-plugin';
+import CopyPlugin from 'copy-webpack-plugin';
 import path from 'path';
 
 module.exports = {
@@ -34,5 +35,13 @@ module.exports = {
     plugins: [
         new ForkTsCheckerWebpackPlugin(),
         new CleanWebpackPlugin(),
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: 'prisma',
+                    to: 'prisma',
+                }
+            ],
+        }),
     ],
 };

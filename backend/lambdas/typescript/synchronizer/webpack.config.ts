e80c85ec7c0f06910fdd6af5c5ca7af526a8c3db
Zicco99 <f.ziccolella@protonmail.com>
2024-05-12 19:03:16 +0200

    removing dist injection

---
+++ backend/lambdas/typescript/synchronizer/webpack.config.ts
@@ -0,0 +1,48 @@
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+import { CleanWebpackPlugin } from 'clean-webpack-plugin';
+import CopyPlugin from 'copy-webpack-plugin';
+import path from 'path';
+
+module.exports = {
+    target: "node18",
+    mode: 'development',
+    context: __dirname,
+    entry: './lambda.ts',
+    externals: ['_http_common', 'encoding'],
+    devtool: 'inline-source-map',
+    resolve: {
+        extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
+    },
+    output: {
+        libraryTarget: 'commonjs2',
+        path: path.join(__dirname, '.webpack'),
+        filename: '[name].js',
+    },
+    module: {
+        noParse: /node_modules/,
+        rules: [
+            {
+                test: /\.(ts|js)x?$/,
+                exclude: /node_modules/,
+                loader: 'esbuild-loader',
+                options: {
+                    loader: 'tsx',
+                    target: 'es2015',
+                },
+            },
+        ],
+    },
+    plugins: [
+        new ForkTsCheckerWebpackPlugin(),
+        new CleanWebpackPlugin(),
+        new CopyPlugin({
+            patterns: [
+                { 
+                    from: 'prisma/client', 
+                    to: 'prisma/client'
+                },
+            ],
+        })
+
+    ],
+};

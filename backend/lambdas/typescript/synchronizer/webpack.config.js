f69306e5d804061a7a9f918051f31ba6541c465a
Zicco99 <f.ziccolella@protonmail.com>
2024-05-18 02:20:18 +0200

    without provide plugin

--- backend/lambdas/typescript/synchronizer/webpack.config.js
+++ backend/lambdas/typescript/synchronizer/webpack.config.js
@@ -3,7 +3,6 @@ const nodeExternals = require('webpack-node-externals');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 module.exports = {
     target: "node",
@@ -61,7 +60,6 @@ module.exports = {
                     to: 'prisma',
                 }
             ],
-        }),
-        new BundleAnalyzerPlugin(), 
+        }), 
     ],
 };

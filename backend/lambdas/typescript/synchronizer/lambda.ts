9f62ec1d59f94fd3335a79e293a487b9dde885d7
Zicco99 <f.ziccolella@protonmail.com>
2024-05-18 14:52:26 +0200

    fixed lambda

--- backend/lambdas/typescript/synchronizer/lambda.ts
+++ backend/lambdas/typescript/synchronizer/lambda.ts
@@ -1,9 +1,9 @@
 import { Handler, Context, Callback } from 'aws-lambda';
-import { PrismaClient, Recipe } from './prisma/client';
+import { PrismaClient } from './prisma/client';
 import axios from 'axios';
 import cheerio from 'cheerio';
 
-import { LambdaClient, InvokeCommand, InvokeCommandInput } from '@aws-sdk/client-lambda';
+import { LambdaClient, InvokeCommand } from '@aws-sdk/client-lambda';
 
 const BASE_URL = 'https://www.giallozafferano.it/ricette-cat/';
 let db_client: PrismaClient | null = null;
@@ -17,47 +17,34 @@ const scrapRecipe = async (url: string): Promise<void> => {
         console.error('Error scraping recipe:', error);
     }
 };
-
-const handler: Handler = async (
-    event: any,
-    context: Context,
-    callback: Callback,
-) => {
-    if (!db_client) {
-        db_client = new PrismaClient();
-        await db_client.$connect();
-    }
-
+const parallelizeScraping = async (context: Context, n_page?: number): Promise<void> => {
     try {
-        if (!event.n_page) {
+        if (!n_page) {
             // Fetch total number of pages
             const response = await axios.get(BASE_URL);
             const htmlContent = response.data;
-            const $ = cheerio.load(htmlContent);
+            const $ = cheerio.load(htmlContent); // Define $ as CheerioAPI
 
             const totalPagesText = $('div.disabled.total-pages').text().trim();
             const numberOfPages = parseInt(totalPagesText);
 
             // Invoke Lambda functions in parallel to scrape multiple pages simultaneously
-            await Promise.all([...Array(numberOfPages).keys()].map(async (i) => {
-                const params: InvokeCommandInput = {
+            const promises = Array.from({ length: numberOfPages - 1 }, async (undefined, i) => {
+                const params = {
                     FunctionName: context.functionName,
                     Payload: JSON.stringify({ 'n_page': i + 1 }),
                 };
                 const command = new InvokeCommand(params);
-                try {
-                    await lambdaClient.send(command);
-                } catch (error) {
-                    console.error('Error invoking Lambda:', error);
-                    // Handle error or retry logic if needed
-                }
-            }));
+                return lambdaClient.send(command);
+            });
+
+            await Promise.all(promises);
         } else {
             // Scraping logic done by each recursive invocation
-            const response = await axios.get(`${BASE_URL}${event.n_page}`);
-            const $ = cheerio.load(response.data);
+            const response = await axios.get(`${BASE_URL}${n_page}`);
+            const $ = cheerio.load(response.data); // Define $ as CheerioAPI
             const recipeLinks: string[] = [];
-            $('.gz-title a').each((index: any, element: any) => {
+            $('.gz-title a').each((index: number, element: any) => { // Use CheerioElement here
                 const recipeLink = $(element).attr('href');
                 if (recipeLink) {
                     recipeLinks.push(recipeLink);
@@ -67,6 +54,25 @@ const handler: Handler = async (
                 await scrapRecipe(link);
             }));
         }
+    } catch (error) {
+        console.error('Error:', error);
+        throw error; // rethrow the error to be caught by the handler
+    }
+};
+
+
+const handler: Handler = async (
+    event: { n_page?: number },
+    context: Context,
+    callback: Callback,
+): Promise<void> => {
+    if (!db_client) {
+        db_client = new PrismaClient();
+        await db_client.$connect();
+    }
+
+    try {
+        await parallelizeScraping(context,event.n_page);
     } catch (error) {
         console.error('Error:', error);
     } finally {
@@ -86,3 +92,4 @@ const handler: Handler = async (
 };
 
 export { handler };
+

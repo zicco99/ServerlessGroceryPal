f8ae94cc2a493858878f0bacb26b02811ca5e73e
Zicco99 <f.ziccolella@protonmail.com>
2024-05-20 09:27:13 +0200

    lambda fixed

--- backend/lambdas/typescript/synchronizer/lambda.ts
+++ backend/lambdas/typescript/synchronizer/lambda.ts
@@ -26,6 +26,7 @@ const scrapRecipe = async (url: string): Promise<void> => {
         const recipeData = await fetchRecipeData(url);
         console.log("Data obtained: ", recipeData);
         if (recipeData.title && recipeData.category && recipeData.ingredients.length > 0 && recipeData.steps.length > 0) {
+            console.log('Saving recipe on DB...');
             saveRecipeOnDB(db_client,recipeData).then(console.log);
         } else {
             throw new Error('Recipe data is not complete')

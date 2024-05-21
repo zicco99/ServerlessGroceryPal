f4aa8fa8d77d719df23a0e966e5062243ee1bef6
Zicco99 <f.ziccolella@protonmail.com>
2024-05-21 09:03:26 +0200

    added secret perms to synchronizer

--- backend/main.py
+++ backend/main.py
@@ -566,7 +566,6 @@ class BackendStack(NestedStack):
             #syncronizer: [Permission.RW_PERM_SCRAP_DB],
             #recipes: [Permission.RW_PERM_SCRAP_DB]
             nestjs_serverless: [Permission.RW_PERM_SCRAP_DB],
-            backend_db_proxy: [Permission.RW_PERM_SCRAP_DB],
             synchronizer: [Permission.RW_PERM_SCRAP_DB],
         }
 

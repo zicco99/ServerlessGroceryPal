7a4f38af8706247c431dcb0e4ff9ed87ba48ae54
Zicco99 <f.ziccolella@protonmail.com>
2024-05-25 22:04:58 +0200

    push

--- backend/lambdas/typescript/claude_chef/src/scrap/recipes.ts
+++ backend/lambdas/typescript/claude_chef/src/scrap/recipes.ts
@@ -1,8 +1,6 @@
 import axios from 'axios';
-import cheerio from 'cheerio';
 
 import { PrismaClient, Recipe } from '../../prisma/client';
-import AWS from 'aws-sdk';
 
 type RecipeData = {
     title: string | null;
@@ -11,47 +9,6 @@ type RecipeData = {
     ingredients: { name: string; quantity: string }[];
     steps: { imageUrl: string | null; explaining: string }[];
 };
-
-async function fetchRecipeData(url: string): Promise<RecipeData> {
-    try {
-        const { data } = await axios.get(url);
-        const $ = cheerio.load(data);
-
-        // Find Title
-        const titleRecipe = $('.gz-title-recipe.gz-mBottom2x').text().trim() || null;
-
-        // Find Category
-        const category = $('.gz-breadcrumb li a').first().text().trim() || null;
-
-        // Find Ingredients
-        const allIngredients: { name: string; quantity: string }[] = [];
-        $('.gz-ingredients .gz-ingredient').each((i, elem) => {
-            const nameIngredient = $(elem).find('a').text().trim();
-            const quantityProduct = $(elem).find('span').contents().first().text().replace(/\s+/g, ' ').trim();
-            if (nameIngredient && quantityProduct) {
-                allIngredients.push({ name: nameIngredient, quantity: quantityProduct });
-            }
-        });
-
-        // Find Image URL
-        const imageURL = $('.gz-featured-image img[data-src]').attr('data-src') || $('#gz-raccomandatore-trigger img').attr('src') || "";
-        
-        // Find Steps
-        const steps: { imageUrl: string | null; explaining: string }[] = [];
-        $('.gz-content-recipe-step').each((i, elem) => {
-            const imageUrl = $(elem).find('img').attr('data-src') || null;
-            const explaining = $(elem).find('p').text().trim();
-            if (explaining) {
-                steps.push({ imageUrl, explaining });
-            }
-        });
-
-        return { title: titleRecipe, category: category, imageUrl: imageURL, ingredients: allIngredients, steps };
-    } catch (error) {
-        console.error('Error fetching the recipe:', error);
-        return { title: null, category: null, imageUrl: null, ingredients: [], steps: [] };
-    }
-}
 
 
 async function saveRecipeOnDB(prisma: PrismaClient | null, recipeData: RecipeData): Promise<Recipe | null> {
@@ -145,5 +102,5 @@ async function saveRecipeOnDB(prisma: PrismaClient | null, recipeData: RecipeDat
 }
 
 
-export { fetchRecipeData, saveRecipeOnDB, RecipeData };
+export { saveRecipeOnDB, RecipeData };
 

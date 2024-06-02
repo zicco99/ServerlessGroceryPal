b4068b511993b6fcceb32cdedeb2b4f8dcaea7a5
Zicco99 <f.ziccolella@protonmail.com>
2024-06-02 16:41:13 +0200

    back to haiku

--- backend/lambdas/typescript/claude_chef/lambda.ts
+++ backend/lambdas/typescript/claude_chef/lambda.ts
@@ -80,10 +80,8 @@ async function askClaudeChef(recipeData: RecipeData, knowledgeBase: ClaudeChefKn
 
         console.log('Claude Context: ', context);
 
-        console.log('User message: ', JSON.stringify(recipeData));
-
         const response = await anthropic.messages.create({
-            model: 'claude-3-sonnet-20240229',
+            model: 'claude-3-haiku-20240229',
             max_tokens: 2080,
             messages: [{
                 role: 'user',

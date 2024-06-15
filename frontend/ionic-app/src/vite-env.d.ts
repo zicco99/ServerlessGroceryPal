0d12e413ae30ed07aecd7374decc359687983e40
Zicco99 <f.ziccolella@protonmail.com>
2024-06-15 14:31:04 +0200

    ionic v8 stable

---
+++ frontend/ionic-app/src/vite-env.d.ts
@@ -0,0 +1,14 @@
+/// <reference types="react-scripts" />
+
+interface ImportMeta {
+    readonly env: {
+      readonly VITE_STACKREGION: string;
+      readonly VITE_USERPOOLID: string;
+      readonly VITE_USERPOOLCLIENTID: string;
+      readonly VITE_USERPOOLDOMAIN: string;
+      readonly VITE_USERPOOLCLIENTCALLBACKURLLOCAL: string;
+      readonly VITE_USERPOOLCLIENTCALLBACKURLREMOTE: string;
+      readonly VITE_USERPOOLCLIENTLOGOUTURLLOCAL: string;
+      readonly VITE_USERPOOLCLIENTLOGOUTURLREMOTE: string;
+    };
+  }

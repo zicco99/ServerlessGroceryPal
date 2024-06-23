9443c7f85aa2657d7fd7e7d0919ff02c10c4ce1f
Zicco99 <f.ziccolella@protonmail.com>
2024-06-24 00:57:29 +0200

    debug

--- backend/api/src/-users/users.service.ts
+++ backend/api/src/-users/users.service.ts
@@ -19,22 +19,21 @@ export class UsersService {
               fridges: true
             }
           });
+
+        if(!user) {
+            user = await this.db_client.user.create({
+                data: {
+                  id
+                }
+              });
+
+            console.log("User created: ", user);
+        }
     }
     else{
         user = await this.db_client.user.findUnique({
             where: { id }
           });
-    }
-
-    if(!user && isOwnerAsking) {
-        console.log("Allocating user in user: ", id);
-      // Cognito space is created but db linked user is missing -> allocate it
-      user = await this.db_client.user.create({
-        data: {
-          id
-        }
-      });
-      console.log("User created: ", user);
     }
 
     return user;

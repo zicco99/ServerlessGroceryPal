a268cb17fb94cb78f16aa62d212300f01b52b963
Zicco99 <f.ziccolella@protonmail.com>
2024-06-23 12:43:32 +0200

    using decorator to specify the request param

--- backend/api/src/utils/cognito.middleware.ts
+++ backend/api/src/utils/cognito.middleware.ts
@@ -43,7 +43,7 @@ export class CheckAuthCognitoMiddleware implements NestMiddleware {
       const user = { id: sub, email, firstname: given_name, lastname: family_name };
       console.log('User:', user);
 
-      req.headers['x-user'] = stringify(user);
+      req.headers['x-user'] = JSON.stringify(user);
       next();
     } catch (error) {
       console.error('Error validating token:', error);
